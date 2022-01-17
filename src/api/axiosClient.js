fd402bfb1c9fa4dffd21f848c281e42ccf113127
Huy Nguyen <huynguyen235023@gmail.com>
2022-01-17 16:23:02 +0700

    Create axiosClient, redux store and set up react-router-dom for project

---
+++ src/api/axiosClient.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+import queryString from 'query-string'
+
+const axiosClient = axios.create({
+	baseURL: 'http://localhost:8000',
+	headers: {
+		'Content-Type': 'application/json',
+	},
+	paramsSerializer: params => queryString.stringify(params),
+})
+
+axiosClient.interceptors.request.use(
+	config => {
+		return config
+	},
+	error => {
+		return Promise.reject(error)
+	}
+)
+
+axiosClient.interceptors.response.use(
+	response => {
+		return response.data
+	},
+	error => {
+		return Promise.reject(error?.response?.data)
+	}
+)
+
+export default axiosClient
